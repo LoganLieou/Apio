cf0a2a95e6ea8441aac793b4510d0f9d3e6e0fe1
logan-lieou <loganj@fedora.xyz>
2021-10-25 15:23:04 -0500

    wow

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import scrapy
 
-class TitleSpider(scrapy.Spider):
-   name = 'medspider1'
+class MedSpider(scrapy.Spider):
+   name = 'medspider'
    start_urls = ["https://pubmed.ncbi.nlm.nih.gov/?term=deep%20learning&page=" + str(x) for x in range(1, 303)]
 
    def parse(self, response):
       for item in response.css('div.docsum-content'):
         yield {
             'title': item.css('a.docsum-title::text').getall(),
-            'discription': item.css('div.full-view-snippet::text').getall()
+            'content': item.css('div.full-view-snippet::text').getall()
          }
